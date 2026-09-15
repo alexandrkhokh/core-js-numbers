ac9dc1d6df9f07ba14cff04aafd4988f667d3b2c
obemgcabazn <obemgghost@ya.ru>
2026-09-15 14:01:30 +0300

    update

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -277,8 +277,12 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i += 1) {
+    sum += i;
+  }
+  return sum;
 }
 
 /**
@@ -292,8 +296,15 @@ function getSumToN(/* n */) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return Number(
+    num
+      .toString()
+      .split('')
+      .reduce((acc, cur) => {
+        return Number(acc) + Number(cur);
+      })
+  );
 }
 
 /**
@@ -307,8 +318,8 @@ function getSumOfDigits(/* num */) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  return !(Math.log2(num) % 1);
 }
 
 /**
@@ -321,8 +332,8 @@ function isPowerOfTwo(/* num */) {
  *   0 => 0
  *   Math.PI / 2 => 1
  */
-function getSine(/* num */) {
-  throw new Error('Not implemented');
+function getSine(num) {
+  return Math.sin(num);
 }
 
 /**
